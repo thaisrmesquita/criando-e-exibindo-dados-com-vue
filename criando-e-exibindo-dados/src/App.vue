511902bf779e1dc700ba63ee937b5d29cd8243e8
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2019-07-06 12:26:47 -0300

    V-model para capturar dados de inputs

--- criando-e-exibindo-dados/src/App.vue
+++ criando-e-exibindo-dados/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <h1 v-once v-text='titulo'></h1>
     <h2>{{ subtitulo }}</h2>
+    <input v-model="subtitulo" type='text' placeholder="Descrição"/>
     <div v-if="tarefas.length <= 0">
       Não há tarefas
     </div>
